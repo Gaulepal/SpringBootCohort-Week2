e91dd4d545893be5506d39640b3f88156b028197
gaulepal <gaulepal@gmail.com>
2025-11-30 00:11:05 +0100

    create  employee

--- src/main/java/com/kumar/week2/controllers/EmployeeController.java
+++ src/main/java/com/kumar/week2/controllers/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.kumar.week2.controllers;
 
+import com.kumar.week2.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
@@ -9,6 +13,12 @@ public class EmployeeController {
   @GetMapping(path = "/")
   public String getMessage() {
     return "Hello World!";
+  }
+
+  // take id as input
+  @GetMapping("/employees/{employeeId}")
+  public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
+    return new EmployeeDTO(1L, "Hora", "go@gm.com", 12, LocalDate.of(2005, 10, 4), true);
   }
 
 }
