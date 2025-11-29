8c1728457a4e15dbc9d2a14fa29fff0e255cc96e
gaulepal <gaulepal@gmail.com>
2025-11-29 23:49:11 +0100

    add getMapping

--- src/main/java/com/kumar/week2/controllers/EmployeeController.java
+++ src/main/java/com/kumar/week2/controllers/EmployeeController.java
@@ -1,7 +1,14 @@
 package com.kumar.week2.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+
+  @GetMapping(path = "/")
+  public String getMessage() {
+    return "Hello World!";
+  }
+
 }
