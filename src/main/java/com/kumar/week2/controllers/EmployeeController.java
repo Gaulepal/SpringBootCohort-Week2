05d0ef8674ebb1cc6519b92b326b2a46246d4dbb
gaulepal <gaulepal@gmail.com>
2025-12-01 22:27:08 +0100

    add ModelMapper

--- src/main/java/com/kumar/week2/controllers/EmployeeController.java
+++ src/main/java/com/kumar/week2/controllers/EmployeeController.java
@@ -1,10 +1,11 @@
 package com.kumar.week2.controllers;
 
-import com.kumar.week2.entities.EmployeeEntity;
+import com.kumar.week2.dto.EmployeeDTO;
 import com.kumar.week2.services.EmployeeService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/employees")
@@ -19,21 +20,19 @@ public class EmployeeController {
 
   // take id as input
   @GetMapping("/{employeeId}")
-  public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id) {
-    return employeeService.getEmployeeById(id).orElse(null);
+  public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId") Long id) {
+    return employeeService.getEmployeeById(id);
   }
 
   // list of the employees -> required false to make it optional -> default is required
-  @GetMapping
-  public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String name) {
-//    return "Hello, age is: " + age + " name is: " + name;
-
-    return employeeRepository.findAll();
-  }
-
-  // create employee
-  @PostMapping
-  public EmployeeEntity createEmployee(@RequestBody EmployeeEntity employeeEntity) {
-    return employeeRepository.save(employeeEntity);
-  }
+//  @GetMapping
+//  public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String name) {
+//    return employeeService.findAll();
+//  }
+//
+//  // create employee
+//  @PostMapping
+//  public EmployeeEntity createEmployee(@RequestBody EmployeeEntity employeeEntity) {
+//    return employeeRepository.save(employeeEntity);
+//  }
 }
--- src/main/java/com/kumar/week2/services/EmployeeService.java
+++ src/main/java/com/kumar/week2/services/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.kumar.week2.services;
 
+import com.kumar.week2.dto.EmployeeDTO;
 import com.kumar.week2.entities.EmployeeEntity;
 import com.kumar.week2.repositories.EmployeeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class EmployeeService {
@@ -10,10 +13,17 @@ public class EmployeeService {
   // inject the EmployeeRepository
   private final EmployeeRepository employeeRepository;
 
+  // constructor generated
   public EmployeeService(EmployeeRepository employeeRepository) {
     this.employeeRepository = employeeRepository;
   }
 
-  public EmployeeEntity getEmployeeById(Long id) {
+  // method
+  public EmployeeDTO getEmployeeById(Long id) {
+    // create Entity
+    EmployeeEntity employeeEntity = employeeRepository.findById(id).orElse(null);
+    // convert to DTO
+    return new EmployeeDTO(Objects.requireNonNull(employeeEntity).getId(), employeeEntity.getName(), employeeEntity.getEmail(), employeeEntity.getAge(), employeeEntity.getDateOfJoining(), employeeEntity.getIsActive());
   }
+
 }
