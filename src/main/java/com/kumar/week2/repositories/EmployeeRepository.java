3e916e1bd6b0fb5c94f6fdb908780c595141259a
gaulepal <gaulepal@gmail.com>
2025-12-01 01:10:17 +0100

    remove elements from TreeMap

--- src/main/java/com/kumar/week2/repositories/EmployeeRepository.java
+++ src/main/java/com/kumar/week2/repositories/EmployeeRepository.java
@@ -3,10 +3,6 @@ package com.kumar.week2.repositories;
 import com.kumar.week2.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-
-  List<EmployeeEntity> findByName(String name);
 
 }
