2ae09941377e232cf3327a629aeabbc7c0f31833
gaulepal <gaulepal@gmail.com>
2025-12-01 01:20:10 +0100

    add Repository annotation

--- src/main/java/com/kumar/week2/repositories/EmployeeRepository.java
+++ src/main/java/com/kumar/week2/repositories/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.kumar.week2.repositories;
 
 import com.kumar.week2.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
 }
