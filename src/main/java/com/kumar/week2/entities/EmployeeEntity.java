76070673a5ae62ec7f5bd20a0e6d67721e7b392a
gaulepal <gaulepal@gmail.com>
2025-12-01 01:43:26 +0100

    add getters and setters

--- src/main/java/com/kumar/week2/entities/EmployeeEntity.java
+++ src/main/java/com/kumar/week2/entities/EmployeeEntity.java
@@ -7,6 +7,53 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "employees")
 public class EmployeeEntity {
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public LocalDate getDateOfJoining() {
+    return dateOfJoining;
+  }
+
+  public void setDateOfJoining(LocalDate dateOfJoining) {
+    this.dateOfJoining = dateOfJoining;
+  }
+
+  public Boolean getActive() {
+    return isActive;
+  }
+
+  public void setActive(Boolean active) {
+    isActive = active;
+  }
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
