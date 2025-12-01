dded3ed1c3abc7de4249c2bcf3137c5ca5ecb0f8
gaulepal <gaulepal@gmail.com>
2025-12-01 01:17:30 +0100

    add repository in the constructor

--- src/main/java/com/kumar/week2/controllers/EmployeeController.java
+++ src/main/java/com/kumar/week2/controllers/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.kumar.week2.controllers;
 
 import com.kumar.week2.dto.EmployeeDTO;
+import com.kumar.week2.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -8,6 +9,13 @@ import java.time.LocalDate;
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController {
+
+  // connect the repository
+  private final EmployeeRepository employeeRepository;
+
+  public EmployeeController(EmployeeRepository employeeRepository) {
+    this.employeeRepository = employeeRepository;
+  }
 
   // take id as input
   @GetMapping("/{employeeId}")
@@ -19,5 +27,6 @@ public class EmployeeController {
   @GetMapping
   public String getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String name) {
     return "Hello, age is: " + age + " name is: " + name;
+
   }
 }
