b4ae7ef473ea45ac67f525aa56f3c37b3b06fee9
gaulepal <gaulepal@gmail.com>
2025-12-01 22:57:21 +0100

    add the Configuration

--- src/main/java/com/kumar/week2/configs/MapperConfig.java
+++ src/main/java/com/kumar/week2/configs/MapperConfig.java
@@ -2,7 +2,9 @@ package com.kumar.week2.configs;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class MapperConfig {
 
   @Bean
