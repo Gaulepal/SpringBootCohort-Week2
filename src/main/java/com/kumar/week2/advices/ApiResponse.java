5b2af2de47b67611b158fac9163e37894b417fad
gaulepal <gaulepal@gmail.com>
2025-12-02 23:50:14 +0100

    call default constructor from another constructor using this()"

---
+++ src/main/java/com/kumar/week2/advices/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.kumar.week2.advices;
+
+import java.time.LocalDateTime;
+
+public class ApiResponse<T> {
+  private T data;
+  private ApiError error;
+  private LocalDateTime timestamp;
+
+  // default constructor that runs at least once while creating the object -> we can create the timestamp
+  public ApiResponse() {
+    this.timestamp = LocalDateTime.now();
+  }
+
+  // for data
+  public ApiResponse(T data) {
+    this();// call default constructor
+    this.data = data;
+  }
+
+  // for error
+  public ApiResponse(ApiError apiError) {
+    this();// call default constructor
+    this.error = apiError;
+  }
+}
