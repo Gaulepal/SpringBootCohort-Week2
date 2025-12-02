e68bab72d0a929a95fa0e2f0b7ecc061de57c7ce
gaulepal <gaulepal@gmail.com>
2025-12-03 00:16:11 +0100

    change the time format

--- src/main/java/com/kumar/week2/advices/ApiResponse.java
+++ src/main/java/com/kumar/week2/advices/ApiResponse.java
@@ -1,13 +1,19 @@
 package com.kumar.week2.advices;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class ApiResponse<T> {
+
+  //  @Pattern(regexp = "hh-mm-ss dd-mm-yyyy")
+  @JsonFormat(pattern = "hh-mm-ss dd-mm-yyyy") // Jackson will format accordingly
   private LocalDateTime timestamp;
+
   private T data;
+
   private ApiError error;
 
   // default constructor that runs at least once while creating the object -> we can create the timestamp
